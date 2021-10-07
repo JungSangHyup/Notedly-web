988a33c50f71b1f6fcc79e508c581727c196ffb3
JungSangHyup <51068026+JungSangHyup@users.noreply.github.com>
2021-10-07 12:32:06 +0900

    application ready for production

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+import Button from '../components/Button';
 
 const Home = () => {
   return (
     <div>
-      <h1>Notedly!</h1>
       <p>This is the home page</p>
-      <ul>
-        <li>
-          <Link to="/mynotes">My Notes</Link>
-        </li>
-        <li>
-          <Link to="favorites">Favorites</Link>
-        </li>
-      </ul>
+      <Button>Click me !</Button>
     </div>
   );
 };
