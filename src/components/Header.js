bb9ff9ca2a72e800c6d164bcd76053c626d0290e
JungSangHyup <51068026+JungSangHyup@users.noreply.github.com>
2021-10-08 11:25:10 +0900

    로그인 구현 부분

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import logo from '../img/logo.svg';
+import { useQuery, gql } from '@apollo/client';
+import { Link, withRouter } from 'react-router-dom';
+import ButtonAsLink from './ButtonAsLink';
 
 const HeaderBar = styled.header`
   width: 100%;
@@ -24,13 +27,31 @@ const UserState = styled.div`
   margin-left: auto;
 `;
 
-const Header = () => {
+const IS_LOGGED_IN = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+
+const Header = props => {
+  const { data, client } = useQuery(IS_LOGGED_IN);
+
   return (
     <HeaderBar>
       <img src={logo} alt="Notedly Logo" height="40" />
       <LogoText>Notedly</LogoText>
+      <UserState>
+        {data.isLoggedIn ? (
+          <ButtonAsLink>Log Out</ButtonAsLink>
+        ) : (
+          <p>
+            <Link to={'/signin'}>Sign In</Link> or{' '}
+            <Link to={'signup'}>Sign Up</Link>
+          </p>
+        )}
+      </UserState>
     </HeaderBar>
   );
 };
 
-export default Header;
+export default withRouter(Header);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,55 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
 
 import Layout from '../components/Layout';
 
 import Home from './home';
 import MyNotes from './mynotes';
 import Favorites from './favorites';
-import NotePage from './note';
+import Note from './note';
 import SignUp from './signup';
+import SignIn from './signin';
+
+const IS_LOGGED_IN = gql`
+  {
+    isLoggedIn @client
+  }
+`;
 
 const Pages = () => {
+  const PrivateRoute = ({ component: Component, ...rest }) => {
+    const { loading, error, data } = useQuery(IS_LOGGED_IN);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error!</p>;
+    return (
+      <Route
+        {...rest}
+        render={props =>
+          data.isLoggedin === true ? (
+            <Component {...props} />
+          ) : (
+            <Redirect
+              to={{
+                pathname: '/signin',
+                state: { from: props.location }
+              }}
+            />
+          )
+        }
+      />
+    );
+  };
+
   return (
     <Router>
       <Layout>
         <Route exact path="/" component={Home} />
-        <Route path="/mynotes" component={MyNotes} />
-        <Route path="/favorites" component={Favorites} />
-        <Route path="/note/:id" component={NotePage} />
+        <PrivateRoute path="/mynotes" component={MyNotes} />
+        <PrivateRoute path="/favorites" component={Favorites} />
+        <Route path="/note/:id" component={Note} />
         <Route path="/signup" component={SignUp} />
+        <Route path="/signin" component={SignIn} />
       </Layout>
     </Router>
   );
